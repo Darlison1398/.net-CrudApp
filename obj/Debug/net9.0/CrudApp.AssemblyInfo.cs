505e881a66757b6d35181fee0305a17985d9750f
Darlison1398 <drsilva123jd@gmail.com>
2025-01-13 13:38:55 -0300

    Nova atualização

--- obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaaf911ba513fcef78d1dd7330657558863aa9a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290f0c30b56a375933c25391ea79bfd20de60e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
